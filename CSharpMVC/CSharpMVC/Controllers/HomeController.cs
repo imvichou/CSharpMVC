290791128c53cfff2bb7ac206faf2ca13844ab94
imvichou <77609971+imvichou@users.noreply.github.com>
2021-02-07 03:59:34 +0800

    CRUD
    ASP.NET MVC Coding

---
+++ CSharpMVC/CSharpMVC/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using CSharpMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSharpMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+            DbToDoEntities = new dbToDoEntities();
+        }
+
+        dbToDoEntities DbToDoEntities;
+
+        public ActionResult Index()
+        {
+            var todos = DbToDoEntities.tToDo.OrderByDescending(x => x.fDate).ToList();
+
+            return View(todos);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string fTitle, string fLevel, DateTime fDate)
+        {
+            var todo = new tToDo();
+            todo.fTitle = fTitle;
+            todo.fLevel = fLevel;
+            todo.fDate = fDate;
+            DbToDoEntities.tToDo.Add(todo);
+            DbToDoEntities.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var todo = DbToDoEntities.tToDo.Where(m => m.fId == id).FirstOrDefault();
+            DbToDoEntities.tToDo.Remove(todo);
+            DbToDoEntities.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
